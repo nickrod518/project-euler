66c550ecddeb2452c390938b29fe46cddd1a7bba
Nick Rodriguez <nickrod518@gmail.com>
2012-09-10 22:58:32 -0300

    Update problem7.cpp
    changed != to <

--- problem7.cpp
+++ problem7.cpp
@@ -8,7 +8,7 @@ int main() {
   bool prime = false;
   unsigned int primeNum = 2;
   
-  while (primeNum != 10001) {
+  while (primeNum < 10001) {
     for (int i = 2; i <= number/2; ++i) {
       if (number%i == 0) {
         number++;
