097da9b09de2bc7b3b1e075243c02b57acca63b2
Nick Rodriguez <nick@gmail.com>
2012-09-25 13:38:29 -0400

    started problem 8

---
+++ problem8.cpp
@@ -0,0 +1,15 @@
+int main {
+
+// hold big number as string
+
+// convert 5 characters at a time to ints
+
+// multiply ints together
+
+// if it's the largest, store it
+
+//move up one character
+
+
+  return 0;
+}
