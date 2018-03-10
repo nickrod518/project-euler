6362acfa82398cdefb295266c6c02fef1afda88f
Rodriguez, Nick <Nick.Rodriguez@dhgllp.com>
2018-03-10 10:06:27 -0500

    Add Python solution for problem 48.

--- problem48.ps1
+++ problem48.ps1
@@ -1,8 +1,8 @@
-# bigint doesn't appear to be large enough to solve this...
+# bigint doesn't appear to be large enough to solve this...in PowerShell. Check Python script.
 [bigint]$sum = 0
 
 for ($i = 1; $i -le 1000; $i++) {
-    [Math]::Pow($i, $i)
+    $sum = $sum + [Math]::Pow($i, $i)
 }
 
 ([string]$sum)[-10..-1] -join ''
